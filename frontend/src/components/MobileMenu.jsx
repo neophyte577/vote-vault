57c342b605d12759d2479a93f5e6602160af609e
neophyte577 <138714913+neophyte577@users.noreply.github.com>
2025-03-29 01:34:02 -0400

    Fix close button in MobileMenu

--- frontend/src/components/MobileMenu.jsx
+++ frontend/src/components/MobileMenu.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
+import { X } from 'lucide-react';
 
 const MobileMenu = ({ menuOpen, setMenuOpen }) => {
 
@@ -14,10 +15,10 @@ const MobileMenu = ({ menuOpen, setMenuOpen }) => {
     >
       <button
         onClick={() => setMenuOpen(false)}
-        className="absolute top-6 right-6 text-paleHoney text-3xl focus:outline-none cursor-pointer"
+        className="absolute top-6 right-6 text-paleHoney focus:outline-none cursor-pointer"
         aria-label="Close Menu"
       >
-        ✖
+        <X className="w-11 h-11 stroke-[2.25] text-paleHoney"/>
       </button>
 
       <HashLink
